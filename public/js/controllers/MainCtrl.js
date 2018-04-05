f168cbc99f622928858caa02fbbe6a5c8ae0f194
dannyo669 <dannyoleary1@gmail.com>
2018-04-05 02:10:17 +0100

    Added calculation support for testcases so they are displayed on table for most entries. Angular select box added to select test case.

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,23 +1,37 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope,$q, JSONModel, REST, MainUtil) {
-    // var test = JSONModel.get()
-    // test.stocks.forEach(function (value) {
-    //     REST.create('/api/stocks', value)
-    // })
+     // var test = JSONModel.get()
+     // test.stocks.forEach(function (value) {
+     //     REST.create('/api/stocks', value)
+     // })
 
 
+    $scope.testCase = [0,1,2,3,4,5];
+
+    $scope.currentCase = 1;
 
     $q.all([
         REST.get('/api/stocks'),
-        REST.get('https://scraper601.herokuapp.com/scrape/test?n=1')
+        REST.get('https://scraper601.herokuapp.com/scrape/test?n='+$scope.currentCase)
     ]).then(function(data) {
+
+        console.log($scope.currentCase);
+
         $scope.allStocks = data[0];
         $scope.testInfo = data[1];
+        $scope.allStocks.sort(function(a,b){
+               var desA = a.description.toLowerCase()
+               var desB = b.description.toLowerCase()
+            if (desA < desB) //sort string ascending
+                return -1
+            if (desA > desB)
+                return 1
+            return 0 //default return value (no sorting)
+            }
+        );
         $scope.allStocks.forEach(function (entry) {
             MainUtil.setUpData(entry, $scope.testInfo)
         })
     });
-
-
      // REST.get('/api/stocks').then(function(data){
      //     $scope.allStocks = data;
      // }).then(function (){
@@ -31,7 +45,55 @@ angular.module('MainCtrl', []).controller('MainController', function($scope,$q, 
      // })
 
 
+    $q.all([
+        REST.get('/api/stocks'),
+        REST.get('https://scraper601.herokuapp.com/scrape/test?n='+$scope.currentCase)
+    ]).then(function(data) {
 
+        console.log($scope.currentCase);
+
+        $scope.allStocks = data[0];
+        $scope.testInfo = data[1];
+        $scope.allStocks.sort(function(a,b){
+                var desA = a.description.toLowerCase()
+                var desB = b.description.toLowerCase()
+                if (desA < desB) //sort string ascending
+                    return -1
+                if (desA > desB)
+                    return 1
+                return 0 //default return value (no sorting)
+            }
+        );
+        $scope.allStocks.forEach(function (entry) {
+            MainUtil.setUpData(entry, $scope.testInfo)
+        })
+    });
+
+    $scope.updateTableData = function () {
+        $q.all([
+            REST.get('/api/stocks'),
+            REST.get('https://scraper601.herokuapp.com/scrape/test?n='+$scope.currentCase)
+        ]).then(function(data) {
+
+            console.log($scope.currentCase);
+
+            $scope.allStocks = data[0];
+            $scope.testInfo = data[1];
+            $scope.allStocks.sort(function(a,b){
+                    var desA = a.description.toLowerCase()
+                    var desB = b.description.toLowerCase()
+                    if (desA < desB) //sort string ascending
+                        return -1
+                    if (desA > desB)
+                        return 1
+                    return 0 //default return value (no sorting)
+                }
+            );
+            $scope.allStocks.forEach(function (entry) {
+                MainUtil.setUpData(entry, $scope.testInfo)
+            })
+        });
+    }
 
 
 
--- public/js/services/MainUtilService.js
+++ public/js/services/MainUtilService.js
@@ -4,10 +4,35 @@ angular.module('MainUtilService', []).factory('MainUtil', function() {
     }
     return{
         setUpData : function (currencyData, queryData) {
-            console.log(queryData)
             if (currencyData.location == 'ise'){
                 //step one. Get purchase price.
                 currencyData.purchasePrice = getPurchasePrice(currencyData)
+                console.log(currencyData)
+                console.log(queryData)
+                if (currencyData.symbol == 'AIBG_I'){
+                    currencyData.price = queryData.ise.data[0].price
+                    currencyData.value = (currencyData.quantity * currencyData.price).toFixed(2)
+                    currencyData.sellCosts = (currencyData.value * 0.01)+1.25
+                    currencyData.gainOrLost = (currencyData.value - currencyData.cost)-currencyData.sellCosts
+                    currencyData.percentGainOrLost = Math.round((currencyData.gainOrLost/currencyData.cost)*100)+"%"
+                }
+                else if (currencyData.symbol == 'BIRG_I'){
+                    currencyData.price = queryData.ise.data[1].price
+                    currencyData.value = (currencyData.quantity * currencyData.price).toFixed(2)
+                    currencyData.sellCosts = (currencyData.value * 0.01)+1.25
+                    currencyData.gainOrLost = (currencyData.value - currencyData.cost)-currencyData.sellCosts
+                    currencyData.percentGainOrLost = Math.round((currencyData.gainOrLost/currencyData.cost)*100)+"%"
+                }
+                else{
+                    currencyData.price = queryData.ise.data[2].price
+                    currencyData.value = (currencyData.quantity * currencyData.price).toFixed(2)
+                    currencyData.sellCosts = (currencyData.value * 0.01)+1.25
+                    currencyData.gainOrLost = (currencyData.value - currencyData.cost)-currencyData.sellCosts
+                    currencyData.percentGainOrLost = Math.round((currencyData.gainOrLost/currencyData.cost)*100)+"%"
+                }
+
+
+                //0,1,2 in an array depends on what type of stock. 0 is AIBG_I. 1 is BIRG_I, 2 is CRH_I
 
                 //step two. Get price now.
             }
